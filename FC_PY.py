e9ebaa4d837dfe56c4c57dabb9141db6116a18ec
Sirius1897 <Sirius97black@gmail.com>
2018-10-14 01:54:41 +0200

    p3

--- FC_PY.py
+++ FC_PY.py
@@ -4,7 +4,9 @@ from navio.lsm9ds1 import *
 print("henllo World")
 MPU = MPU9250()
 
+MPU.initialize(100, "BITS_DLPF_CFG_256HZ_NOLPF2")
 MPU.set_acc_scale("BITS_FS_2G")
+MPU.set_gyro_scale("BITS_FS_250DPS")
 
 p = MPU.getMotion6()
 
