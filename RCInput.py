d746f91454e5495918dffd97c756fb61c0e5737e
Sirius1897 <Sirius97black@gmail.com>
2018-10-13 23:39:02 +0200

    ADD Navio2 Ex

---
+++ RCInput.py
@@ -0,0 +1,14 @@
+import sys
+import time
+
+import navio.rcinput
+import navio.util
+
+navio.util.check_apm()
+
+rcin = navio.rcinput.RCInput()
+
+while (True):
+    period = rcin.read(2)
+    print(period)
+    time.sleep(1)
