67aa4c6a1ba7269c0f7208bd44da793871e0cd72
Sirius1897 <Sirius97black@gmail.com>
2018-10-14 01:30:46 +0200

    py3

--- LED.py
+++ LED.py
@@ -16,32 +16,32 @@ while (True):
 
     led.setColor('Black')
     print("LED is Black")
-    time.sleep(0.05)
+    time.sleep(0.5)
 
     led.setColor('Red')
     print("LED is Red")
-    time.sleep(0.05)
+    time.sleep(0.5)
 
     led.setColor('Green')
     print("LED is Green")
-    time.sleep(0.05)
+    time.sleep(0.5)
 
     led.setColor('Blue')
     print("LED is Blue")
-    time.sleep(0.05)
+    time.sleep(0.5)
 
     led.setColor('Cyan')
     print("LED is Cyan")
-    time.sleep(0.05)
+    time.sleep(0.5)
 
     led.setColor('Magenta')
     print("LED is Magenta")
-    time.sleep(0.05)
+    time.sleep(0.5)
 
     led.setColor('Yellow')
     print("LED is Yellow")
-    time.sleep(0.05)
+    time.sleep(0.5)
 
     led.setColor('White')
     print("LED is White")
-    time.sleep(0.05)
+    time.sleep(0.5)
