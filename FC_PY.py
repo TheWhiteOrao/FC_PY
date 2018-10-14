9e794e0344be75daf674bcc178f476a6f3bb2f69
Sirius1897 <Sirius97black@gmail.com>
2018-10-14 02:07:00 +0200

    p3

--- FC_PY.py
+++ FC_PY.py
@@ -4,6 +4,8 @@ from navio.lsm9ds1 import *
 print("henllo World")
 MPU = MPU9250()
 
+MPU.WriteReg(1, 1)
+MPU.ReadReg(1)
 MPU.initialize(100, "BITS_DLPF_CFG_188HZ")
 MPU.set_acc_scale("BITS_FS_2G")
 MPU.set_gyro_scale("BITS_FS_250DPS")
