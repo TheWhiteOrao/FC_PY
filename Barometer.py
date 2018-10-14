db3ba6bacd07d5d1c23b3e1b5f56928d1c0acf62
Sirius1897 <Sirius97black@gmail.com>
2018-10-14 23:28:34 +0200

    py3

--- Barometer.py
+++ Barometer.py
@@ -19,16 +19,16 @@ baro = navio.ms5611.MS5611()
 baro.initialize()
 
 while(True):
-	baro.refreshPressure()
-	time.sleep(0.01) # Waiting for pressure data ready 10ms
-	baro.readPressure()
+    baro.refreshPressure()
+    time.sleep(0.01)  # Waiting for pressure data ready 10ms
+    baro.readPressure()
 
-	baro.refreshTemperature()
-	time.sleep(0.01) # Waiting for temperature data ready 10ms
-	baro.readTemperature()
+    baro.refreshTemperature()
+    time.sleep(0.01)  # Waiting for temperature data ready 10ms
+    baro.readTemperature()
 
-	baro.calculatePressureAndTemperature()
+    baro.calculatePressureAndTemperature()
 
-	print "Temperature(C): %.6f" % (baro.TEMP), "Pressure(millibar): %.6f" % (baro.PRES)
+    print("Temperature(C): %.6f" % (baro.TEMP), "Pressure(millibar): %.6f" % (baro.PRES))
 
-	time.sleep(1)
+    time.sleep(1)
