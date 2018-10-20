cefa41e63e39beb6579fa152ddca0108f6770385
Sirius1897 <Sirius97black@gmail.com>
2018-10-20 17:08:32 +0200

    red

--- RCInput.py
+++ RCInput.py
@@ -9,6 +9,24 @@ navio.util.check_apm()
 rcin = navio.rcinput.RCInput()
 
 while (True):
-    period = rcin.read(1)
-    print(period)
+    period_can_0 = rcin.read(0)
+    period_can_1 = rcin.read(1)
+    period_can_2 = rcin.read(2)
+    period_can_3 = rcin.read(3)
+    period_can_4 = rcin.read(4)
+    period_can_5 = rcin.read(5)
+    period_can_6 = rcin.read(6)
+    period_can_7 = rcin.read(7)
+    period_can_8 = rcin.read(8)
+
+    print("CAN_0: %.6f" % period_can_0,
+          "CAN_1: %.6f" % period_can_1,
+          "CAN_2: %.6f" % period_can_2,
+          "CAN_3: %.6f" % period_can_3,
+          "CAN_4: %.6f" % period_can_4,
+          "CAN_5: %.6f" % period_can_5,
+          "CAN_6: %.6f" % period_can_6,
+          "CAN_7: %.6f" % period_can_7,
+          "CAN_8: %.6f" % period_can_8,
+          )
     time.sleep(0.01)
