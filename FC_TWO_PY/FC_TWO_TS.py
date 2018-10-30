d0813300476cf6f7e454e1bc008c8b383d296128
Sirius1897 <Sirius97black@gmail.com>
2018-10-30 01:25:37 +0100

    ds

--- FC_TWO_PY/FC_TWO_TS.py
+++ FC_TWO_PY/FC_TWO_TS.py
@@ -1,5 +1,8 @@
 # import time as tv
 # while True:
 #     print(tv.time())
-
-print(": {0:20} :".format(1343423.345123))
+#
+# print(": {0:20} :".format(1343423.345123))
+g = {0: 1}
+g[0] += 1
+print(g)
